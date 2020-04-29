c4d648ea68c6ac85facb20fc395ddb64b9e5ad63
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-04-29 18:04:16 +0200

    Add controller and view

---
+++ app/controllers/gender_stories_controller.rb
@@ -0,0 +1,6 @@
+class GenderStoriesController < ApplicationController
+  before_action :authorize_global
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+match 'gender_stories', to: 'gender_stories#index', as: 'gender_stories', via: [:get]
