f7fe6514ab1c10540996393789b6c8858e59733e
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-05-11 16:39:15 +0200

    Number of users

--- lib/redmine_equality_plan/redmine_patch/models/user_patch.rb
+++ lib/redmine_equality_plan/redmine_patch/models/user_patch.rb
@@ -3,16 +3,20 @@ module RedmineEqualityPlan
     module UserPatch
       def self.included(base)
         base.class_eval do
+          scope :in_company, proc {
+            in_company_custom
+          }
+
           scope :male, proc {
             gender_custom_field_id = CustomField.gender_custom_field_id
             male_ids = CustomField.find(gender_custom_field_id).custom_values.where(value: 'male').map(&:customized_id)
-            User.where(type: 'User').active.where(id: male_ids)
+            User.in_company.where(id: male_ids)
           }
 
           scope :female, proc {
             gender_custom_field_id = CustomField.gender_custom_field_id
             female_ids = CustomField.find(gender_custom_field_id).custom_values.where(value: 'female').map(&:customized_id)
-            User.where(type: 'User').active.where(id: female_ids)
+            User.in_company.where(id: female_ids)
           }
 
           def gender
