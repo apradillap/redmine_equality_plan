51bddae017b137b5808b08d3435c9ab5a0e077d7
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-09-04 15:40:00 +0200

    Force fail test

--- test/unit/gender_story_test.rb
+++ test/unit/gender_story_test.rb
@@ -4,7 +4,7 @@ class GenderStoryTest < ActiveSupport::TestCase
   def test_initialize
     gender_story = GenderStory.new
 
-    assert_equal(gender_story.female_count, 0)
+    assert_equal(gender_story.female_count, 1)
     assert_equal(gender_story.male_count, 0)
   end
 end
