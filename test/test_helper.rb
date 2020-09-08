9d8ddfb910cc2c772bdf2c7ba9ad9a54e8a7b9e6
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-09-08 15:10:54 +0200

    Modify folders

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'test'
+end
 
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
