0f43c92853c2ccf9b8cbc77d73b3ccf4fec89f84
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-09-08 18:22:34 +0200

    #50 - Add tests to check gender

--- test/unit/gender_story_test.rb
+++ test/unit/gender_story_test.rb
@@ -7,4 +7,22 @@ class GenderStoryTest < ActiveSupport::TestCase
     assert_equal(gender_story.female_count, 0)
     assert_equal(gender_story.male_count, 0)
   end
+
+  def test_assign_gender_to_female_user
+    u = User.new(:firstname => "Maria", :lastname => "user")
+    u.login = "create_email_address_os"
+    u.mail = "osdefaultemail@somenet.foo"
+    assert u.save
+
+    assert_equal(u.gender, 'female')
+  end
+
+  def test_assign_gender_to_male_user
+    u = User.new(:firstname => "Michael", :lastname => "user")
+    u.login = "create_email_address_os"
+    u.mail = "osdefaultemail@somenet.foo"
+    assert u.save
+
+    assert_equal(u.gender, 'male')
+  end
 end
