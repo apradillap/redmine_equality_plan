988f92763bf5961e756f838a813115c60837e7a6
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-09-11 01:34:24 +0200

    :recycle: #63 - Refactor code

--- app/controllers/equality_plans_controller.rb
+++ app/controllers/equality_plans_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class EqualityPlansController < ApplicationController
-  before_action :authorize_global, :define_months
+  before_action :authorize_global
+
+  before_action :define_months, :groups, only: [:index]
 
   def index
     @groups = Group.all
@@ -30,5 +32,58 @@ class EqualityPlansController < ApplicationController
     end
 
     @months = dates.map { |m| m.first.to_s[0..6] }
+  end
+
+  def groups
+    groups = Group.left_joins(:users).group(:id).where("users.status=1").order('COUNT(users.id) DESC')
+    female_hash={}
+    male_hash={}
+
+    groups.each do |group|
+      female_hash[group.id] = ((User.active.female.where("users.id IN (SELECT gu.user_id FROM groups_users gu WHERE gu.group_id=#{group.id})").count.to_f/group.users.count)*100).round(2)
+      male_hash[group.id] = ((User.active.male.where("users.id IN (SELECT gu.user_id FROM groups_users gu WHERE gu.group_id=#{group.id})").count.to_f/group.users.count)*100).round(2)
+    end
+
+    female_hash = female_hash.each { |k, v| female_hash[k].nan? ? 0 : female_hash[k] }
+    male_hash = male_hash.each { |k, v| male_hash[k].nan? ? 0 : male_hash[k] }
+
+    @feminised_hash1 = female_hash.select {|k,v| v == 100}
+    @masculinized_hash1 = male_hash.select { |k,v| @feminised_hash1.keys.include?(k) }
+
+    @masculinized_hash2 = male_hash.select {|k,v| v == 100}
+    @feminised_hash2 = female_hash.select { |k,v| @masculinized_hash2.keys.include?(k) }
+  end
+
+  def ages
+    @ages = ['16-19', '20-29', '30-39', '40-49', '50-59', '60-80']
+
+    @female_age_ranges = [User.for_age_range(16, 19).female.size,
+                          User.for_age_range(20, 29).female.size,
+                          User.for_age_range(30, 39).female.size,
+                          User.for_age_range(40, 49).female.size,
+                          User.for_age_range(50, 59).female.size,
+                          User.for_age_range(60, 80).female.size]
+
+    @male_age_ranges = [User.for_age_range(16, 19).male.size,
+                        User.for_age_range(20, 29).male.size,
+                        User.for_age_range(30, 39).male.size,
+                        User.for_age_range(40, 49).male.size,
+                        User.for_age_range(50, 59).male.size,
+                        User.for_age_range(60, 80).male.size]
+  end
+
+  def salary
+    @total_male = 0
+    @total_female = 0
+    User.female.custom_with_salary.each do |user|
+      @total_female += user.salary
+    end
+
+    User.male.custom_with_salary.each do |user|
+      @total_male += user.salary
+    end
+
+    @average_male_salary = total_male/User.male.custom_with_salary.size
+    @average_female_salary = total_female/User.female.custom_with_salary.size
   end
 end
