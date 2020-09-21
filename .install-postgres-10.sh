f704f0aeb9f55bb2334ecb1ad992f3caf5bb4141
Adrián Pradilla Pórtoles <adrianpradilla@gmail.com>
2020-09-21 23:39:07 +0200

    #75 Delete file

--- .install-postgres-10.sh
+++ .install-postgres-10.sh
@@ -10,6 +10,5 @@ sudo apt-get install -q postgresql-10 postgresql-client-10
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 
 echo "Restarting Postgres 10"
-sudo rm /usr/local/var/postgres/postmaster.pid
 sudo /etc/init.d/postgresql restart
 sleep 15
